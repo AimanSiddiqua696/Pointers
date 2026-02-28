529f95b4058f3cfe02802581309798a44b6ca9cf
Aiman Siddiqua <siddiquaaimen@gmail.com>
2026-02-28 16:31:53 +0500

    NULL Pointer

--- code.cpp
+++ code.cpp
@@ -1,11 +1,20 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int a = 10;
-    int* ptr = &a;
-    int** parptr = &ptr;
-    cout << &ptr << endl;
-    cout << parptr << endl;
-    cout << *(&a) <<endl;
-    return 0;
+    // int a = 10;
+    // int* ptr = &a;
+    // int** parptr = &ptr;//**store the address of another pointer 
+    // cout << &ptr << endl;//0x1dcc9ffa88
+    // cout << parptr << endl;//0x1dcc9ffa88
+    // // dereference operater
+    // cout << *(&a) <<endl; //10
+    // cout << *(ptr) <<endl;//10
+    // int** parPtr = &ptr;
+    // cout << *(parPtr) <<endl;//0xa60bdffe0c
+    // cout << (ptr) <<endl;//0xa60bdffe0c
+    // cout << **(parPtr) <<endl;//10
+    //null pointer
+    int** ptr = NULL;
+    // cout<< ptr << endl; //memory address null, output will be 0
+    // cout<< *ptr << endl;
 }
