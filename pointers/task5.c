af166ecd688bec17500b2c7135ee7e7d601c105c
Aiman Siddiqua <siddiquaaimen@gmail.com>
2026-03-01 16:51:29 +0500

    Array Pointer

---
+++ pointers/task5.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+    // int arr[] ={1,2,3,4};
+    // int* p = arr;
+    // // printf("%d",*(p+2));
+    // printf("%d%d", *p, *(p+2));
+    int arr[] = {5,10,15};
+    printf("%d", *(arr + 1));
+}
---
+++ pointers/tempCodeRunnerFile.c
@@ -0,0 +1,4 @@
+int arr[] ={1,2,3,4};
+    // int* p = arr;
+    // // printf("%d",*(p+2));
+    // printf("%d%d", *p, *(p+2));
