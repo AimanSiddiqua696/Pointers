e3c393699106739b8f3b1912b938241bc9a92471
Aiman Siddiqua <siddiquaaimen@gmail.com>
2026-03-02 21:50:00 +0500

    swapping

---
+++ pointers/task6.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+// Function definition OUTSIDE main
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main() {
+    int x = 5, y = 10;
+
+    swap(&x, &y);
+
+    printf("x = %d, y = %d", x, y);
+
+    return 0;
+}
