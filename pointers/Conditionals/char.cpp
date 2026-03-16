475f863b76ae0dcd708b6f2367cdfec9996cb18f
Aiman Siddiqua <siddiquaaimen@gmail.com>
2026-03-16 22:48:01 +0500

    Add program to check whether a character is lowercase or uppercase

--- pointers/Conditionals/char.cpp
+++ pointers/Conditionals/char.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int ch;
+    char ch;
     cout<<"Enter Character :";
     cin>>ch;
-    if(ch >=a )
+    if(ch >='a' && ch <='z'){
+        cout<< "Lowercase";
+             
+    } else{
+        cout<<"Uppercase";
+    }
+    return 0;
 }
