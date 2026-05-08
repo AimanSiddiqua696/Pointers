6880642cdf73a299cec6395b82f28d65e19b045f
Aiman Siddiqua <siddiquaaimen@gmail.com>
2026-05-08 16:16:03 +0500

    Adding all the tasks of BST

---
+++ pointers/Stack/Queue/BST/task3.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+// Node structure
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+    Node(int value){
+        data = value;
+        left = right = NULL;
+    }
+};
+
+int findMinimum(Node* root){
+while (root -> left != NULL)
+root =  root -> left;
+ return root->data;
+}
+
+
+//insert
+Node* insert(Node* root, int value){
+    if(root == NULL){
+     return new Node(value);
+    }
+    if(value < root->data){
+        root -> left = insert(root->left, value);
+
+    } else if(value > root -> data){
+        root ->right = insert(root->right, value);
+    }
+    return root;
+}
+int main(){
+    Node* root = NULL;
+    root = insert(root, 50);
+    insert(root, 30);
+    insert(root, 70);
+    insert(root, 20);
+    insert(root, 40);
+    insert(root, 60);
+    insert(root, 80);
+
+
+   cout<<" found minimum: " << findMinimum(root);
+
+return 0;
+}
